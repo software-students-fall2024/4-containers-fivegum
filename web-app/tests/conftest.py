22917f47d2df330161eb3e4c08f920cc2c614d02
nehamagesh <neha3magesh@gmail.com>
2024-11-18 12:18:47 -0500

    fix docstrings

--- web-app/tests/conftest.py
+++ web-app/tests/conftest.py
@@ -1,9 +1,11 @@
+"""
+Configuring the webapp tests
+"""
 import pytest
-from app import app
+from ..app import app
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
--- web-app/tests/test_web_app.py
+++ web-app/tests/test_web_app.py
@@ -1,21 +1,27 @@
+"""
+This is the tests for the webapp
+"""
 from io import BytesIO
-from app import app, audio_collection, metadata_collection
 from unittest.mock import patch
+from ..app import app, audio_collection, metadata_collection
 import pytest
 
 
 def test_index(client):
+    """Test that the index route '/' works."""
     response = client.get("/")
     assert response.status_code == 200
 
 
 def test_record_route(client):
+    """Test that the record route '/' works."""
     response = client.get("/record")
     assert response.status_code == 200
 
 
 @patch("app.audio_collection.insert_one")
 def test_missing_filename(mock_audio, client):
+    """Test missing filename"""
     file_info = (BytesIO(b"fake_audio_data"), "test_audio.wav")
     response = client.post("/upload-audio", data={"audio": file_info})
     assert response.status_code == 400
@@ -26,6 +32,7 @@ def test_missing_filename(mock_audio, client):
 @patch("app.requests.get")
 @patch("app.metadata_collection.insert_one")
 def test_upload_audio(mock_req, mock_md, mock_audio, client):
+    """Test uploading audio"""
     mock_audio.return_value.inserted_id = "mock_file"
     mock_md.return_value.acknowledged = True
     mock_req.return_value.ok = True
@@ -46,6 +53,7 @@ def test_upload_audio(mock_req, mock_md, mock_audio, client):
 @patch("app.audio_collection.insert_one")
 @patch("app.metadata_collection.insert_one")
 def test_db_failure(mock_md, mock_audio, client):
+    """test a failing db"""
     mock_audio.return_value.inserted_id = None
 
     data = {
@@ -59,6 +67,7 @@ def test_db_failure(mock_md, mock_audio, client):
 
 @patch("app.audio_collection.insert_one")
 def test_audio_missing(mock_audio, client):
+    """test a missing audio"""
     response = client.post("/upload-audio", data={"name": "test_audio"})
     assert response.status_code == 400
     assert not mock_audio.called
